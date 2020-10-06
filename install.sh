bca184352c718433c2e5070cad2ddfa9ee505730
dev2033 <dev.elop.02@mail.ru>
2020-10-06 19:16:36 +0300

    first commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+base_python_interpreter=""
+project_path=`pwd`
+
+read -p "Python interpreter: " base_python_interpreter
+`$base_python_interpreter -m venv env`
+source env/bin/activate
+pip install -U pip
+pip install -r requirements.txt
+
